6ee62c9cf4cabb404fd13be460772f73505f37d5
Facundo DE LOS RIOS <facujofre96@gmail.com>
2023-05-02 20:14:35 -0300

    add styles in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-function App () {
+export function App () {
     return (
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img className="tw-followCard-avatar" src="https://unavatar.io/github/37t?fallback=https://avatar.vercel.sh/37t?size=400" alt="Avatar aleatorio" />
+                <div className="tw-followCard-info">
+                    <strong>Leopoldo Garcia</strong>
+                    <span className="tw-followCard-infoUserName">@polgarcia</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className="tw-followCard-button">
+                    Seguir
+                </button>
+            </aside>
+        </article>
     )
 }
