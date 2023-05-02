feff94b53e9a77150f70aae2a2ad94d0b073fd26
Facundo DE LOS RIOS <facujofre96@gmail.com>
2023-05-02 20:47:53 -0300

    add component reutlization

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
 export function App () {
     return (
-        <article className="tw-followCard">
-            <header className="tw-followCard-header">
-                <img className="tw-followCard-avatar" src="https://unavatar.io/github/37t?fallback=https://avatar.vercel.sh/37t?size=400" alt="Avatar aleatorio" />
-                <div className="tw-followCard-info">
-                    <strong>Leopoldo Garcia</strong>
-                    <span className="tw-followCard-infoUserName">@polgarcia</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className="tw-followCard-button">
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <>
+            <TwitterFollowCard userName='midudev' name='Miguel Angel Duran'/>
+            <TwitterFollowCard userName='polgarcia' name='Leopoldo Garcia'/>
+            <TwitterFollowCard userName='mouredev' name='Brais Moure'/>
+        </>
     )
 }
