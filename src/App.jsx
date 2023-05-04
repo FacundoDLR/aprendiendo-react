874bc8867b883670313378cb85a1120a83e78b12
Facundo DE LOS RIOS <facujofre96@gmail.com>
2023-05-04 20:48:31 -0300

    add rendering of list users

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 import { TwitterFollowCard } from "./TwitterFollowCard";
 import './App.css'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: true,
+    },
+    {
+        userName: 'polgarcia',
+        name: 'Leopoldo Garcia',
+        isFollowing: false,
+    },
+    {
+        userName: 'jonmircha',
+        name: 'Jonathan MirCha',
+        isFollowing: true,
+    },
+    {
+        userName: 'mouredev',
+        name: 'Brais Moure',
+        isFollowing: false,
+    },
+]
+
 export function App () {
     return (
         <section className="tw-followCard-section">
-            <TwitterFollowCard  userName='midudev' name='Miguel Angel Duran' initialIsFollowing={true}/>
-            <TwitterFollowCard  userName='polgarcia' name='Leopoldo Garcia'/>
-            <TwitterFollowCard  userName='mouredev' name='Brais Moure'/>
-            <TwitterFollowCard />
+            {
+                users.map(user => {
+                    const {userName, name, isFollowing} = user
+                    return (
+                        <TwitterFollowCard
+                            key={userName}
+                            userName={userName}
+                            name={name}
+                            initialIsFollowing={isFollowing}
+                        />
+                    )
+                })
+            }
         </section>
     )
 }
